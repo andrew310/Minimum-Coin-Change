b75b556e2d56cecddbefef4ff9cc014909c40aba
Andrew Brown <andrewbrown600@gmail.com>
2016-01-27 10:59:19 -0800

    added 2nd algorithm
    added changegreedy

--- makechange.py
+++ makechange.py
@@ -1,12 +1,14 @@
 __author__ = 'Andrew.Brown'
 
 
-def makeChange(coinValueList,value):
+def changeDP(coinValueList,value):
     #create table for coinList * value
     minCoins = [[0 for x in range(value+1)]for x in range(len(coinValueList))]
     #fill first line of array with increments of 1
     for i in range(value+1):
         minCoins[0][i] = i
+
+    returnThis = []
 
     #outer loop for coin denomination array
     for i in range(1, len(coinValueList)):
@@ -20,11 +22,27 @@ def makeChange(coinValueList,value):
             #than we did it with the previous coin
             elif j>= coinValueList[i]:
                 minCoins[i][j] = min(minCoins[i-1][j], minCoins[i][j-coinValueList[i]]+1)
-    return minCoins[len(coinValueList)-1][value]
+
+        returnThis.append(minCoins[i][j]/coinValueList[i])
+
+    return (returnThis, minCoins[len(coinValueList)-1][value])
 
 
-coins = [1,10,25,50]
+def changeGreedy(coins,value):
+    pocket = []
+    sum = 0
+    #start with highest value coin (assumes sorted array of denominations)
+    for i in range((len(coins)-1), -1, -1):
+        temp = value/coins[i]
+        pocket += [1,] * temp
+        value -= coins[i] * temp
+        if temp != 0:
+            sum +=1
 
-results = makeChange(coins, 40)
+    return (pocket,sum)
+
+coins = [1,2,4,8]
+
+results = changeDP(coins, 15)
 
 print results
