1f1acc1f1da246fec3cb31d12d4f5d81e66cd28d
Andrew Brown <andrewbrown600@gmail.com>
2016-01-28 15:11:22 -0800

    cleaned up code
    added comment headers, also fixed greedy algorithm

--- makechange.py
+++ makechange.py
@@ -1,5 +1,10 @@
 __author__ = 'Andrew.Brown'
 
+##############################################################
+# changeDP(coinValueList, value)
+# uses the dynamic programming approach to solve the min coin problem in O(nA) running time
+# inputs: an array containing denominations of coins, an int to find change for
+# returns: a tuple containg array with count of coins used, and the final answer
 def changeDP(coinValueList,value):
     #create table for coinList * value
     minCoins = [[0 for x in range(value+1)]for x in range(len(coinValueList))]
@@ -27,52 +32,56 @@ def changeDP(coinValueList,value):
             elif j>= coinValueList[i]:
                 minCoins[i][j] = min(minCoins[i-1][j], minCoins[i][j-coinValueList[i]]+1)
                 coinsUsed[i][j] += 1
-
+	#list to be filled by getCoins function
     newList = [0]*len(coinValueList) 
     getCoins(len(coinValueList)-1, value, coinValueList, coinsUsed, newList)
+    return (newList, minCoins[len(coinValueList)-1][value])
 
-    return (minCoins[len(coinValueList)-1][value])
 
-#function to take usedCoins array and break it down to see how many of each coins used
-def getCoins(i, j, denom, used, returnThis):
+
+##############################################################
+# getCoins(i, j, denom, used, returnThis)
+# This function will fill the returnThis array with the number of times each denomination has been used
+# inputs: length of coin denomination list, value of change needed, coin denomination list, coinsUsed array, and array to be filled with counts of denoms used
+# returns: n/a, uses reference to fill the returnThis
+def getCoins(i, j, denom, used, fill_This):
     if j < 0 or i < 0:
         return
  
+    #if current denom was used at least once in this spot
     if used[i][j] >0:
-        returnThis[i] +=1
-        #print denom[i]
-        #print used[i][j]
+        fill_This[i] +=1
         if j-denom[i]<denom[i]:
             #change denomination.
-            getCoins(i-1, j-denom[i], denom, used, returnThis)
+            getCoins(i-1, j-denom[i], denom, used, fill_This)
 
         else:
             #same denomination
-           # print "same denom"
-            getCoins(i, j-denom[i], denom, used, returnThis)
-            
-            
+            getCoins(i, j-denom[i], denom, used, fill_This)
+ 
+	#if current denom no longer used, go to next denom
     else:
-        print "hi"
-        getCoins(i-1, j, denom, used, returnThis)
-        
-
-	print returnThis
+        getCoins(i-1, j, denom, used, fill_This)
 
 
-
+##############################################################
+# changeGreedy(coins, value)
+# This function uses the linear approach to solve the min coin problem
+# however since it is "greedy" ie always starts with highest coin values, can result in higher "minumum" than other algorithms
+# inputs: an array containing coin denominations, an int containing value to find change for
+# returns: an array with a count of how many times each denomination was used, and an int with the sum
 def changeGreedy(coins,value):
-    pocket = []
+    pocket = [0] * len(coins)
     sum = 0
     #start with highest value coin (assumes sorted array of denominations)
     for i in range((len(coins)-1), -1, -1):
         temp = value/coins[i]
-        pocket += [1,] * temp
+        pocket[i] += 1
         value -= coins[i] * temp
         if temp != 0:
             sum +=1
 
-    return (pocket,sum)
+    return (pocket,sum)  
 
 
 
